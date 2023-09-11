92685ab275b87c8954906e6e76e51bea517bca95
tjclutter <tjclutter@outlook.com>
2023-09-11 07:10:50 -0400

    final commit with comments

--- CT_Module1.cpp
+++ CT_Module1.cpp
@@ -6,34 +6,44 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
+//include statements
 #include <iostream>
 #include <string>
+//standard namespace declaration
 using namespace std;
 
+//main function
 int main() {
+	//variable declarations
 	string firstName;
 	string lastName;
 	string streetAdress;
 	string city;
 	string zipCode;
 
+	// prompt user for first name and store in variable
 	cout << "Enter first name: ";
 	cin >> firstName;
 	cout << endl;
+	// prompt user for last name and store in variable
 	cout << "Enter last name: ";
 	cin >> lastName;
 	cout << endl;
+	// prompt user for street address and store in variable
 	cout << "Enter street address: ";
 	cin.ignore();
 	getline(cin, streetAdress);
 	cout << endl;
-
+	// prompt user for city and store in variable
 	cout << "Enter City: ";
 	getline(cin, city);
 	cout << endl;
+	//prompt user for zip code and store invariable
 	cout << "Enter zip code: ";
 	cin >> zipCode;
+	//output to user full name and home address
 	cout << "Name: " << firstName << " " << lastName << endl;
 	cout << "Home address: " << streetAdress << " " << city << " " << zipCode;
+	//main function return statement
 	return 0;
 }
